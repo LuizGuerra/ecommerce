9eb4a6b8b0a3c9bdc07f6ea9837b23c16ca7b1ad
luizguerra <luizpedrofg@gmail.com>
2020-09-18 20:19:08 -0300

    first commit

---
+++ ecommerce/ecommerce/ecommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecommerceApp.swift
+//  ecommerce
+//
+//  Created by Luiz Pedro Franciscatto Guerra on 18/09/20.
+//
+
+import SwiftUI
+
+@main
+struct ecommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
