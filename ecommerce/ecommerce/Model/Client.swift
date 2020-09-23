89d3ffb4c79c55331c316c694e2a7716d3b4dae0
luizguerra <luizpedrofg@gmail.com>
2020-09-23 11:32:21 -0300

    Client model

--- ecommerce/ecommerce/Model/Client.swift
+++ ecommerce/ecommerce/Model/Client.swift
@@ -5,4 +5,12 @@
 //  Created by Luiz Pedro Franciscatto Guerra on 22/09/20.
 //
 
-import Foundation
+import SwiftUI
+
+struct Client {
+    var name: String
+    var userName: String
+    var password: String
+    var cpf:  String
+    var address: String
+}
