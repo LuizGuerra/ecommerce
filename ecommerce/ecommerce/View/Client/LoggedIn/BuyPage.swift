226b46dace82b6326c418ab5099e38343f025051
luizguerra <luizpedrofg@gmail.com>
2020-09-23 11:34:34 -0300

    Buy page view

--- ecommerce/ecommerce/View/Client/LoggedIn/BuyPage.swift
+++ ecommerce/ecommerce/View/Client/LoggedIn/BuyPage.swift
@@ -10,19 +10,58 @@ import SwiftUI
 struct BuyPage: View {
     @Binding var isLoggedIn: Bool
     @State var buyPage: Bool = false
-    @State var itemsInCart: Bool = false
     @State private var buyButtonColor: Color = Color.red
+    
+    @State private var buyProductSheet = false
+    
+    var client: Client
+    @ObservedObject var data: DataManagerViewModel
+    @ObservedObject var shoppingCart = ShoppingCart()
+    
     var body: some View {
         VStack {
             NavigationView {
                 VStack {
                     Form {
                         Section {
-                            Text("Here we will show you the items to add!")
-                                .padding()
+                            List(data.productList, id: \.id) { product in
+                                NavigationLink(destination: BuyProductPage(cart: shoppingCart, product: product)) {
+                                    BuyCard(product: product)
+                                        .padding()
+                                }
+                            }
                         }
                     }
-                }.navigationBarTitle("Hello, user!")
+                    Section {
+                        Button(action: {
+                            buyPage.toggle()
+                        }, label: {
+                            VStack {
+                                if shoppingCart.products.count > 0 {
+                                    HStack {
+                                        Image("shopping-cart")
+                                            .resizable()
+                                            .frame(width: 30, height: 30)
+                                            .accentColor(.blue)
+                                        Text("Total: " + getPrice())
+                                            .padding(8)
+                                            .accentColor(.black)
+                                    }
+                                }
+                                HStack {
+                                    Spacer()
+                                    Text("Buy")
+                                        .fontWeight(.bold)
+                                        .padding(.vertical)
+                                        .accentColor(.white)
+                                    Spacer()
+                                }.background(
+                                    shoppingCart.products.count == 0 ? Color.red : Color.green
+                                )
+                            }
+                        })
+                    }
+                }.navigationBarTitle(client.name.isEmpty ? "Hello!" : "Hello, \(client.name.components(separatedBy: " ")[0])!")
                 .navigationBarItems(trailing: Button(action: {
                     isLoggedIn.toggle()
                 }, label: {
@@ -32,29 +71,36 @@ struct BuyPage: View {
                         .padding()
                 }))
             }
-            Button(action: {
-                itemsInCart.toggle()
-                withAnimation(.easeInOut, {
-                    buyButtonColor = itemsInCart ? .green : .red
-                })
-            }, label: {
-                HStack {
-                    Spacer()
-                    Text("Buy")
-                        .fontWeight(.bold)
-                        .padding(.vertical)
-                        .accentColor(.white)
-                    Spacer()
-                }
-                .background(buyButtonColor)
-            })
-        }
+        }.sheet(isPresented: $buyPage, content: {
+            FinalizeBuyView(shoppingCart: shoppingCart, data: data, finalizeBuySheet: $buyPage)
+        })
+    }
+    
+    private func getPrice() -> String {
+        let total = shoppingCart.getTotal()
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        formatter.locale = Locale(identifier: "en_US")
+        return formatter.string(from: total as NSNumber) ?? "Could not calculate"
     }
 }
 
 struct BuyPage_Previews: PreviewProvider {
     @State private static var loggedIn = true
+    @ObservedObject private static var data = DataManagerViewModel()
+    static let client = Client(name: "Luiz Guerra", userName: "LuizGuerra", password: "123456", cpf: "", address: "")
     static var previews: some View {
-        BuyPage(isLoggedIn: $loggedIn)
+        viewWithoutItem
+    }
+    static var viewWithoutItem: some View {
+        BuyPage(isLoggedIn: $loggedIn, client: client, data: data)
+    }
+    static var viewWithItem: some View {
+        let view = BuyPage(isLoggedIn: $loggedIn, client: client, data: data)
+        var product = data.productList[0]
+        product.ammount = 1
+        view.shoppingCart.products.append(product)
+        return view
     }
 }
