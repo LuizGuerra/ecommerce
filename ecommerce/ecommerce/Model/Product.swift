1186b5b31ed68105a0b43d4655491b2e5d16e60c
luizguerra <luizpedrofg@gmail.com>
2020-09-23 11:32:30 -0300

    Product model

--- ecommerce/ecommerce/Model/Product.swift
+++ ecommerce/ecommerce/Model/Product.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct Product {
+    var id: UUID
+    var name: String
+    var price: Float
+    var ammount: Int
+    
+    init(name: String, price: Float, ammount total: Int) {
+        self.id = UUID()
+        self.name = name
+        self.price = price
+        self.ammount = total
+    }
+    
+}
